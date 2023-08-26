50d9f40be5acbffd79904e794fbdd4193b34c06a
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-08-26 13:50:44 +0300

    init: initial config

---
+++ src/main/java/com/ske/skeecommerce/SkeecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ske.skeecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeecommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkeecommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ske/skeecommerce/SkeecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ske.skeecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeecommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
